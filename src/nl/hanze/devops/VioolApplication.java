899e67c5eecc3ecbf7b1aa7c3d6d1b1c12a801da
Raymond Loman <raymond@carpago.nl>
2020-11-17 09:24:14 +0100

    Add demo for loops

---
+++ src/nl/hanze/devops/VioolApplication.java
@@ -0,0 +1,21 @@
+package nl.hanze.devops;
+
+public class VioolApplication {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[]{1, 2, 3};
+
+        // Enhanced for (for ... each in talen als JavaScript en Perl/Ruby)
+        for(int n: numbers) {
+            System.out.println(n);
+        }
+
+        System.out.println("Hieronder de basic for loop =>");
+
+        // Basic for (dan moet je zelf het werk doen)
+        for(int i = 0;i<numbers.length;i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
